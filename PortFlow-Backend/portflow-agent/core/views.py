fbfd49294a9361e280349ef7e79b6cc743f65c29
SheriffMudasir <sheriffmudasir2021@gmail.com>
2025-11-23 14:48:52 +0100

    Remove backend folder to replace with submodule

--- PortFlow-Backend/portflow-agent/core/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-"""Django views for the core application.
-
-Currently this module does not define any HTTP views but serves as a
-placeholder for future view implementations.
-"""
